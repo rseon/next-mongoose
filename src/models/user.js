c385744111d22b234ec7c008aa81f587369b8e07
rseon <remi.seon@gmail.com>
2023-02-12 09:11:18 +0100

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String
+})
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
